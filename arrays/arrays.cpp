854d7ca35d96e18483a9d58c199c4f64e9a640a9
Hello-himesh <nagahimeshvuppala@gmail.com>
2025-02-06 22:42:12 +0530

    commiting c++ files to git

---
+++ arrays/arrays.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x[]={1,2,3,4,5,6};//initialuzing &declaring an array
+    cout<<"here's your list:"<<" "<<x<<endl;
+    x[4]=0;//changing its value
+    cout<<"changed one:"<<" "<<x<<endl;
+    return 0;
+} 
---
+++ hello/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string name;
+    cout<<"what do u go by?:";
+    cin>>name;
+    cout<<"namaste"<<" "<<name<<endl;
+    return 0;
+}
