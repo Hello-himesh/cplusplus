cc66bac17b7bb172cf6b125fcb0633ac381e244d
Hello-himesh <nagahimeshvuppala@gmail.com>
2025-02-12 19:24:39 +0530

    changes

--- conditions_loops/loops.cpp
+++ conditions_loops/loops.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int main(){
     int x=5;
     for(x>0;x--;){
-        cout<<"its the "<<x<<"th time"<<endl;
-        cout<<"********************"<<endl;
-    while(x>0){
+        cout<<"its the "<<x<<"thh time"<<endl;
+    cout<<"********************"<<endl;
+    }
+    while(x>1)
+    {
         cout<<"its the "<<x<<"th time"<<endl;
         x--;
         }
-    
-    }
  }
