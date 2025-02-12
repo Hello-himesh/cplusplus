30431887edec8b5f78d7624ee2150a3009222a7d
Hello-himesh <nagahimeshvuppala@gmail.com>
2025-02-12 15:24:39 +0530

    adding files

--- conditions_loops/loops.cpp
+++ conditions_loops/loops.cpp
@@ -5,9 +5,11 @@ int main(){
     int x=5;
     for(x>0;x--;){
         cout<<"its the "<<x<<"th time"<<endl;
-    }
- }
-
- void main(){
+        cout<<"********************"<<endl;
+    while(x>0){
+        cout<<"its the "<<x<<"th time"<<endl;
+        x--;
+        }
     
+    }
  }
